87d2e5e32f485a7e276c07f8dc5f47e5a133250c
DarshikaManral <d.manral@uu.nl>
2021-03-24 17:08:53 +0100

    other simulations for planktonic and non- planktonic behaviour study

---
+++ Simulations/kernels/utilities.py
@@ -0,0 +1,24 @@
+import math
+
+def DayoftheYear(particle, fieldset, time):
+    #     print("INITI time:%f, tod:%f, doy:%f " % (time, particle.tod, particle.doy))
+    # update time of the day
+    if particle.tod + particle.dt >= 86400:  # 24 hours completed
+        particle.tod = particle.tod - 86400
+        # update year if needed and day of the year
+        if (particle.is_leap_year and particle.doy + 1 > 366) or (
+                not particle.is_leap_year == 0 and particle.doy + 1 > 365):
+            particle.doy = 1
+            particle.current_year += 1
+            # update leap year status
+            particle.is_leap_year = math.fmod(particle.current_year, 4) == 0 and (
+                    math.fmod(particle.current_year, 100) != 0 or math.fmod(particle.current_year, 400) == 0)
+        else:
+            particle.doy += 1
+
+    particle.tod += particle.dt
+    print("FINAL time:%f, tod:%f, doy:%f " % (time, particle.tod, particle.doy))
+
+def PreventThroughSurfaceError(particle, fieldset, time):
+    if particle.depth<1:
+        particle.depth=1
